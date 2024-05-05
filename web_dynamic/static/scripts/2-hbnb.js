3fe8e2790a9e43c143be63f1a811414b27d1d9fb
SaidLamghari <said.lmg@gmail.com>
2024-05-05 03:58:57 -0700

    web_dynamic

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,36 +1,36 @@
-dy(function() {
-	    // Variable to store checked amenities
-	//     var checkedAmenities = {};
-	//
-	//         // Listen for changes on each input checkbox tag
-	//             $('input[type="checkbox"]').change(function() {
-	//                     var amenityId = $(this).data('id');
-	//                             var amenityName = $(this).data('name');
-	//
-	//                                     // If the checkbox is checked, store the Amenity ID in the variable
-	//                                             if ($(this).is(':checked')) {
-	//                                                         checkedAmenities[amenityId] = amenityName;
-	//                                                                 } else {
-	//                                                                             // If the checkbox is unchecked, remove the Amenity ID from the variable
-	//                                                                                         delete checkedAmenities[amenityId];
-	//                                                                                                 }
-	//
-	//                                                                                                         // Update the h4 tag inside the Amenities div with the list of checked Amenities
-	//                                                                                                                 var amenitiesList = Object.values(checkedAmenities).join(", ");
-	//                                                                                                                         $('#amenities-list').text(amenitiesList);
-	//                                                                                                                             });
-	//                                                                                                                             });
-	//
-	//                                                                                                                             $.ajax({
-	//                                                                                                                               url: 'http://0.0.0.0:5001/api/v1/status/',
-	//                                                                                                                                 type: 'GET',
-	//                                                                                                                                   dataType: 'json',
-	//                                                                                                                                     success: function (json) {
-	//                                                                                                                                         $('#api_status').addClass('available');
-	//                                                                                                                                           },
-	//
-	//                                                                                                                                             error: function (xhr, status) {
-	//                                                                                                                                                 console.log('error ' + status);
-	//                                                                                                                                                   }
-	//
-	//                                                                                                                                                   });
+$(document).ready(function() {
+    // Variable to store checked amenities
+    var checkedAmenities = {};
+
+    // Listen for changes on each input checkbox tag
+    $('input[type="checkbox"]').change(function() {
+        var amenityId = $(this).data('id');
+        var amenityName = $(this).data('name');
+
+        // If the checkbox is checked, store the Amenity ID in the variable
+        if ($(this).is(':checked')) {
+            checkedAmenities[amenityId] = amenityName;
+        } else {
+            // If the checkbox is unchecked, remove the Amenity ID from the variable
+            delete checkedAmenities[amenityId];
+        }
+
+        // Update the h4 tag inside the Amenities div with the list of checked Amenities
+        var amenitiesList = Object.values(checkedAmenities).join(", ");
+        $('#amenities-list').text(amenitiesList);
+    });
+});
+
+$.ajax({
+  url: 'http://0.0.0.0:5001/api/v1/status/',
+  type: 'GET',
+  dataType: 'json',
+  success: function (json) {
+    $('#api_status').addClass('available');
+  },
+
+  error: function (xhr, status) {
+    console.log('error ' + status);
+  }
+
+});
